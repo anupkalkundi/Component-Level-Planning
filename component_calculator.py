d9253808e518ebe883543e503084b0ad342f172b
anupkalkundi <anupkalkundi759@gmail.com>
2026-05-23 15:43:36 +0530

    component_calculator.py

--- component_calculator.py
+++ component_calculator.py
@@ -1341,7 +1341,7 @@ def show_component_calculator(conn, cur):
                     length_value,
                     width_value,
                     thickness_value,
-                    "Total Quantity": first_row["Total Quantity"],
+                    total quantity: first_row["Total Quantity"],
                     round_value=True
                 )
 
